9455dea55421c8d9e84a3c21d0e64cc0fae76315
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2020-09-03 11:41:18 -0400

    links added to projects repositories

--- js/projects_collection.js
+++ js/projects_collection.js
@@ -1,44 +1,51 @@
 const projectsCollection = [
   {
     title: "React Pomodoro Clock",
-    url: "https://jleonardo007.github.io/pomodoro-clock",
     imgPath: "./projects-images/pomodoro-clock.png",
+    projectUrl: "https://jleonardo007.github.io/pomodoro-clock",
+    projectRepository: "https://github.com/jleonardo007/pomodoro-clock",
   },
   {
     title: "React Calculator",
-    url: "https://jleonardo007.github.io/react-calculator",
     imgPath: "./projects-images/calculator.png",
+    projectUrl: "https://jleonardo007.github.io/react-calculator",
+    projectRepository: "https://github.com/jleonardo007/react-calculator",
   },
   {
     title: "React Drum Machine",
-    url: "https://jleonardo007.github.io/react-drum-machine",
     imgPath: "./projects-images/drum-machine.png",
+    projectUrl: "https://jleonardo007.github.io/react-drum-machine",
+    projectRepository: "https://github.com/jleonardo007/react-drum-machine",
   },
   {
     title: "React Markdown Previewer",
-    url: "https://jleonardo007.github.io/markdown-previewer",
     imgPath: "./projects-images/markdown-previewer.png",
+    projectUrl: "https://jleonardo007.github.io/markdown-previewer",
+    projectRepository: "https://github.com/jleonardo007/markdown-previewer",
   },
   {
     title: "React Random Qoutes Machine",
-    url: "https://jleonardo007.github.io/randomquotesmachine/",
     imgPath: "./projects-images/quotes-machine.png",
+    projectUrl: "https://jleonardo007.github.io/randomquotesmachine/",
+    projectRepository: "https://github.com/jleonardo007/randomquotesmachine",
   },
-
   {
     title: "Coming Soon Page",
-    url: "https://codepen.io/jleonardo007/pen/vYENmKB",
     imgPath: "./projects-images/count-down2.png",
+    projectUrl: "https://codepen.io/jleonardo007/pen/vYENmKB",
+    projectRepository: null,
   },
   {
     title: "Coming Soon Page",
-    url: "https://codepen.io/jleonardo007/pen/xxbGWOV",
     imgPath: "./projects-images/count-down1.png",
+    projectUrl: "https://codepen.io/jleonardo007/pen/xxbGWOV",
+    projectRepository: null,
   },
   {
     title: "Survey Form",
-    url: "https://codepen.io/jleonardo007/pen/mdbMGPo",
     imgPath: "./projects-images/survey-form.png",
+    projectUrl: "https://codepen.io/jleonardo007/pen/mdbMGPo",
+    projectRepository: null,
   },
 ];
 
