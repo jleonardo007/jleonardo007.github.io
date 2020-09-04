6cbd030e928db417a03059cd9bb4848f894e9ae5
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2020-09-04 05:31:31 -0400

    adde new project card layout

--- js/ui.js
+++ js/ui.js
@@ -1,14 +1,45 @@
 import projectsCollection from "./projects_collection.js";
 
-const projects = document.querySelector(".projects-section");
+const projects = document.querySelector(".projects-container");
 const indicator = document.getElementById("indicator");
 
 export default class UI {
   addProjects = (index) => {
-    const project = projectsCollection[index];
+    const item = projectsCollection[index];
+
+    const ProjectBackground = `
+      background: url(${item.imgPath});
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center;
+    `;
+
     projects.innerHTML = `
-            
-        `;
+            <article class="project-item">
+              <div class="project-item__img" style="${ProjectBackground}">
+                <div class="img__background-color">
+                  <a 
+                    href="
+                      ${
+                        item.projectRepository === null
+                          ? "#"
+                          : item.projectRepository
+                      }"
+                    target="_blank" 
+                    class="project-item__repository-link">
+                    View on <span class="iconify" data-icon="mdi:github-circle" data-inline="true">
+                  </a>
+                </div>
+              </div>
+              <a 
+                href="${item.projectUrl}"
+                target="_blank" 
+                class="project-item__title"
+                >
+                  ${item.title}
+              </a>
+            </article>
+          `;
   };
 
   toggleSections = (hideSection, showSection) => {
